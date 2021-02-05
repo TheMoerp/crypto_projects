321c3441fe30f70e0151136a76d7395acaff55e4
Matthias Hottgenroth <matthias.hottgenroth@icloud.com>
2021-02-05 18:25:43 +0100

    improved eea-fix

--- expanded_euclidean_algorithm/eea.py
+++ expanded_euclidean_algorithm/eea.py
@@ -5,13 +5,13 @@ def eea(mod, num):
     t = [0, 1]
     q = [0]
     i = 1
-    #print("Runde {}: r0 = {}, r1 = {}, t0 = {}, t1 = {}, q0 = {}".format(i, r[0], r[1], t[0], t[1], q))
+    #print("Round {}: r0 = {}, r1 = {}, t0 = {}, t1 = {}, q0 = {}".format(i, r[0], r[1], t[0], t[1], q))
     while True:
         i += 1
         r.append(r[i - 2] % r[i - 1])
         q.append((r[i - 2] - r[i]) / r[i - 1])
         t.append(t[i - 2] - q[i - 1] * t[i - 1])
-        #print("Runde {}: r{} = {}, t{} = {}, q{} = {}".format(i, i, r[i], i, t[i], i - 1 , q[i - 1]))
+        #print("Round {}: r{} = {}, t{} = {}, q{} = {}".format(i, i, r[i], i, t[i], i - 1 , q[i - 1]))
         if r[i] == 0:
             break
     #print("---------------------------")
