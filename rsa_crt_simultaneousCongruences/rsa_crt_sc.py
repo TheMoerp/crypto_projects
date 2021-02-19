3da4a52c521eecb383e9106b637bf6faed4f3a70
Matthias Hottgenroth <matthias.hottgenroth@icloud.com>
2021-02-19 16:26:21 +0100

    added intermediate results

--- rsa_crt_simultaneousCongruences/rsa_crt_sc.py
+++ rsa_crt_simultaneousCongruences/rsa_crt_sc.py
@@ -2,14 +2,18 @@
 c1 = 1786058302
 c2 = 521332323
 c3 = 2806221682
+#print("\nc1: {}\nc2: {}\nc3: {}\n".format(c1, c2, c3))
+
 
 # Exponent e
 e = 3
+#print("e: {}\n".format(e))
 
 # Modulo ni
 n1 = 3748150753
 n2 = 3012222769
 n3 = 3715158301
+#print("n1: {}\nn2: {}\nn3: {}\n".format(n1, n2, n3))
 
 # Berechnung des EEA
 def eea(mod, num):
@@ -31,22 +35,29 @@ def eea(mod, num):
 
 # Berechnung von N
 N = n1 * n2 * n3
+#print("N: {}\n".format(N))
 
 # Berechnung von Ni & Reduzierung von Ni mit ni
 N1 = N // n1
 N2 = N // n2
 N3 = N // n3
+#print("N1: {}\nN2: {}\nN3: {}\n".format(N1, N2, N3))
+
+
 
 # Berechnung der Multiplikativen Inversen von Ni
 invN1 = int(eea(n1, N1))
 invN2 = int(eea(n2, N2))
 invN3 = int(eea(n3, N3))
+#print("invN1: {}\ninvN2: {}\ninvN3: {}\n".format(invN1, invN2, invN3))
 
 # Berechnung der simultanen Kongruenz s
 c = ((c1 * invN1 * N1) + (c2 * invN2 * N2) + (c3 * invN3 * N3)) % N
+#print("c: {}\n".format(c))
 
 # Berechnung des Klartext m in Dezimalschreibweise
 m = round(c ** (1 / e))
+#print("m: {}\n".format(m))
 
 # Berechne Klartext aus m
 m = str(m)
@@ -55,4 +66,4 @@ mParts = [tmpM[i][::-1] for i in range(len(tmpM))]
 decText = ''.join([chr(int(mParts[i])) for i in range(len(mParts))])
 
 # Gebe den Klartext von m aus
-print("Der Klartext lautet: {}".format(decText))
+print("Der Klartext lautet: {}\n".format(decText))
