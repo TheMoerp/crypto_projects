affe8386bc9810dcdbe93ddbd9ae26476871f0a8
Matthias Hottgenroth <matthias.hottgenroth@icloud.com>
2021-03-15 20:06:22 +0100

    seperated eea and bsgs

--- sources/baby_step_giant_step/babyS_giantS.py
+++ sources/baby_step_giant_step/babyS_giantS.py
@@ -1,20 +1,4 @@
-def eea(mod, num):
-    r = [mod, num]
-    t = [0, 1]
-    q = [0]
-    i = 1
-    while True:
-        i += 1
-        r.append(r[i - 2] % r[i - 1])
-        q.append((r[i - 2] - r[i]) / r[i - 1])
-        t.append(t[i - 2] - q[i - 1] * t[i - 1])
-        if r[i] == 0:
-            break
-    invNum = t[i - 1]
-    while invNum < 0:
-        invNum = invNum + mod
-    return invNum
-
+from eea import eea
 
 def babystep_giantstep(alpha, beta, m, g):
     giant_alpha = int(eea(g, (alpha**m)%g))
