3e7e11d290c371ec797cd199bf8cf2edbd380b00
Matthias Hottgenroth <matthias.hottgenroth@icloud.com>
2021-03-29 17:38:44 +0200

    m compution correction

--- sources/algorithms/baby_step_giant_step/babyS_giantS.py
+++ sources/algorithms/baby_step_giant_step/babyS_giantS.py
@@ -2,8 +2,8 @@ from math import ceil, sqrt
 from eea import eea
 
 def babystep_giantstep(alpha, beta, g):
-    m = ceil(sqrt(g))
-    giant_alpha = int(eea(g, (alpha ** m) % g))
+    m = ceil(sqrt(g - 1))
+    giant_alpha = eea(g, (alpha ** m) % g)
     babystep_list = list(map(lambda x : (alpha ** x) % g, range(m)))
     giantstep_list = list(map(lambda x : ((giant_alpha ** x) * beta) % g, range(m)))
 
@@ -19,7 +19,7 @@ def babystep_giantstep(alpha, beta, g):
 def main():
     alpha = int(input('alpha: '))
     beta = int(input('beta: '))
-    g = int(input('g: '))
+    g = int(input('g (Prim): '))
     babystep_giantstep(alpha, beta, g)
 
 if __name__ == "__main__":
